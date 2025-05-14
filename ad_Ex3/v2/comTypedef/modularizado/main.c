cae57f3f03273d08007c541f775475f5e4caed21
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2025-05-14 15:17:33 -0300

    Create main.c

---
+++ ad_Ex3/v2/comTypedef/modularizado/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Cad.h"
+
+Cad* criarCadastro(int N) {
+    if (N <= 0) return NULL;
+
+    Cad *vetor = (Cad*) malloc(N * sizeof(Cad));
+    if (vetor == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < N; i++) {
+        printf("Cadastro %d:\n", i + 1);
+
+        limparEntrada();
+
+        printf("Nome completo:\t");
+        vetor[i].nome = lerString();
+
+        printf("Idade:\t");
+        scanf("%d", &vetor[i].idade);
+
+        limparEntrada();
+
+        printf("Rua:\t");
+        vetor[i].local.logradouro = lerString();
+
+        printf("Numero:\t");
+        scanf("%d", &vetor[i].local.numeroRua);
+    }
+
+    return vetor;
+}
+
+int main() {
+    int N;
+    printf("Digite o número de cadastros: ");
+    scanf("%d", &N);
+
+    Cad *pessoas = criarCadastro(N);
+
+    printf("\n--- Dados cadastrados ---\n");
+    for (int i = 0; i < N; i++) {
+        printf("Pessoa %d:\n", i + 1);
+        printf("Nome: %s\n", pessoas[i].nome);
+        printf("Idade: %d\n", pessoas[i].idade);
+        printf("Rua: %s\n", pessoas[i].local.logradouro);
+        printf("Numero da rua: %d\n\n", pessoas[i].local.numeroRua);
+    }
+
+    liberarCadastro(pessoas, N);
+    return 0;
+}
